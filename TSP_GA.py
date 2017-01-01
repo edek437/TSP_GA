d332ab7b3c940aa6fa20bf37d24eef1d53812f45
edek437 <adrianwykrzykowski@gmail.com>
2017-01-01 16:43:22 +0100

    mutate, print input args, extra input options

--- TSP_GA.py
+++ TSP_GA.py
@@ -3,6 +3,9 @@ import argparse
 import copy
 import random
 import traceback
+from operator import itemgetter
+import logging
+import sys
 
 
 class GA(object):
@@ -22,6 +25,7 @@ class GA(object):
                 cost_matrix.append(copy.deepcopy(cost_mat_line))
         return cost_matrix
 
+    # generate initial population for GA algorithm
     def _generate_random_population(self):
         pop_size = self.args.population_size
         for i in xrange(0, pop_size):
@@ -43,9 +47,11 @@ class GA(object):
     def _generate_next_population(self):
         pass
 
+    # return two chromosomes picked by roulette wheel selection
     def _roulette_wheel_select(self):
         pass
 
+    # return two chromosomes picked by rank selection
     def _rank_select(self):
         pass
 
@@ -55,17 +61,23 @@ class GA(object):
     def _er_crossover(self, parent1, parent2):
         pass
 
-    def _mutate(self):
-        pass
+    def _mutate(self, chromosome):
+        if random.random() >= self.args.mutation_probability:
+            chromosome_len = len(chromosome)
+            rand_ind1 = random.randint(0, chromosome_len)
+            rand_ind2 = random.randint(0, chromosome_len)
+            chromosome[rand_ind1], chromosome[rand_ind2] = chromosome[rand_ind2], chromosome[rand_ind1]
 
     def run(self):
         print self.graph
         print "-----------------"
-        print self.population
         for it in xrange(0, self.args.iterations):
+            print self.population
+            print "-----------------"
+            self._generate_next_population()
             pass
 
-        return None
+        return min(self.population, key=itemgetter(0))
 
 
 if __name__ == "__main__":
@@ -74,20 +86,26 @@ if __name__ == "__main__":
         parser.add_argument('graph_path', type=str)
         parser.add_argument('--iterations', type=int, default=100)
         parser.add_argument('--population_size', type=int, default=40)
-        parser.add_argument('--crosover_probability', type=float, default=1.0)
+        parser.add_argument('--selection_method', type=str, choices=['roulette_wheel', 'rank'], default='roulette_wheel')
+        parser.add_argument('--crossover_method', type=str, choices=['pmx', 'er'], default='pmx')
+        parser.add_argument('--crossover_probability', type=float, default=1.0)
         parser.add_argument('--mutation_probability', type=float, default=0.5)
         parser.add_argument('--elitism', type=bool, default=True)
         parser.add_argument('--elitism_nbr', type=int, default=2)
 
         args = parser.parse_args()
+        print "\n------------------------------------------------------------"
+        print "                   Running TSP GA for args                  "
+        print "------------------------------------------------------------"
+        print vars(args)
+        sys.exit(0)
         tsp_ga = GA(args)
-        tsp_ga.run()
-
+        best_path = tsp_ga.run()
         print "\n------------------------------------------------------------"
         print "                     Results                                "
         print "------------------------------------------------------------"
-        # print "\nBest path = %s" % (best_path_vec,)
-        # print "\nBest path cost = %s\n" % (best_path_cost,)
+        print "\nBest path = %s" % (best_path[1])
+        print "\nBest path cost = %s\n" % (best_path[0])
 
     except Exception, e:
         print "exception: " + str(e)
